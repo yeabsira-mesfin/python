b952c7ab84e4168547ffaf9c77353e97c14e9087
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-01-17 16:29:58 -0500

    logic and loop

---
+++ WeekOne/lloop.py
@@ -0,0 +1,22 @@
+# for i in range(1,6):
+#     if i % 2 == 0:
+#         print(f"{i} is even")
+#     else:
+#         print(f"{i} is odd")
+# count  = 1
+
+# for i in range(1,10):
+#     if i %3 == 0:
+#         count +=1
+    
+# print(count)
+
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+for i in range(5):
+    if i == 3:
+        break
+    print(i)
