7f51db2a1f9ac9c1a2126ef7bbe0d72bb06a8f0c
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-01-17 14:48:32 -0500

    a

---
+++ WeekOne/decision.py
@@ -0,0 +1,39 @@
+x = int(input("Please enter your number: "))
+if 5 < x < 10:
+    print("X is greater than 5 and less than 10")
+else:
+    print("X is not in the range")
+# else:
+#     print("X is smaller or equal to 5")
+# grade = float(input("Please enter your grade: "))
+
+# if grade >= 94:
+#     print("You got an A")
+# elif grade >= 90:
+#     print("You got an A-")
+# elif grade >=87:
+#     print("You got a B+")
+# elif grade >= 84:
+#     print("You got a B")
+# elif grade >= 80:
+#     print("You got a B-")
+# elif grade >= 77:
+#     print("You got a C+")
+# elif grade >= 74:
+#     print("You got a C")
+# elif grade >= 70:
+#     print("You got a C-")
+# elif grade >= 67:
+#     print("You got a D+")
+# elif grade >= 64:
+#     print("You got a D")
+# elif grade >= 60:
+#     print("You got a D-")
+# else:
+#     print("You got an F")
+
+name = ""
+if name:
+    print("Has a name")
+else:
+    print("No name")
