eba5c65b18145d7e5cabb46e6b41c8cb0b18d273
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-01-28 15:52:09 -0500

    dictionary

---
+++ WeekThree/dictionary.py
@@ -0,0 +1,9 @@
+student_list = {'Name':'Yeabsira', 'Age':26, 'Degree':'Graduate'}
+print(student_list['Age'])
+print(student_list)
+student_list['Age'] = 31
+print(student_list['Age'])
+del student_list['Age']
+print(student_list)
+# student_list.clear()
+# print(student_list)
