23f9c76517c7cb5f3f8b76685aaafe739aa67e2f
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-01-28 18:43:10 -0500

    Dictionary detailed

--- WeekThree/dictionary.py
+++ WeekThree/dictionary.py
@@ -1,9 +1,84 @@
-student_list = {'Name':'Yeabsira', 'Age':26, 'Degree':'Graduate'}
-print(student_list['Age'])
-print(student_list)
-student_list['Age'] = 31
-print(student_list['Age'])
-del student_list['Age']
-print(student_list)
+from pprint import pprint
+# student_list = {'Name':'Yeabsira', 'Age':26, 'Degree':'Graduate'}
+# print(student_list['Age'])
+# print(student_list)
+# student_list['Age'] = 31
+# print(student_list['Age'])
+# del student_list['Age']
+# print(student_list)
 # student_list.clear()
 # print(student_list)
+# temp = student_list['Age']
+# student_list['Degree'] = student_list['Age']
+# student_list['Age'] = temp
+# print(student_list)
+
+# squares = {1:2, 2:3, 4:5, 6:7}
+
+# print(squares.pop(4))
+# print(squares)
+# print(squares.popitem())
+# print(squares)
+# del squares[2]
+# print(squares)
+# print(squares.clear())
+# print(squares)
+
+# gz_dictionary = {'lol': "Laugh out loud",
+#                     'idk':"I don't know",
+#                     'fyi':"For your information",
+#                     'ASAP':"As soon as possible"
+#                     }
+# print(gz_dictionary)
+# print(gz_dictionary["lol"])
+
+continue_function = 'y'
+# while continue_function == 'y':
+#     input_key = input('What would you like to translate?')
+#     print(gz_dictionary[input_key.upper()])
+#     continue_function = input('Do you wanna try another? (y/n)')
+
+# slist1 = {'Yeabsira':26,
+#           'Lucas':9,
+#           'Zema':7,
+#           'Jody':2}
+# print(slist1)
+# slist2 = sorted(slist1)
+# print(slist2)
+
+# student_list = {1:{"Name":'Yeabsira','Age':26,"Degree":"Graduate"},
+#                 2:{"Name":'Yisu', 'Age':18,"Degree":"Masters"},
+#                 3:{"Name":"Kida", "Age":18,"Degree":"Masters"},
+#                 4:{"Name":"John", "Age":19, "Degree":"Masters"},
+#                 5:{"Name":"Zema", "Age":8, "Degree":"Masters"}}
+# print(student_list[2])
+# student_list[5]["Age"] = 23
+# print(student_list[5]["Age"])
+# i = len(student_list)
+# print(i)
+# record_numbers = int(input("How many records do you wanna add?"))
+
+# while continue_function == 'y':
+    
+#     for i in range(start,start + record_numbers):
+#         start = max(student_list.keys()) + 1
+#         student_list[i]= {}
+#         name = input('Please put your name: ')
+#         age = int(input('Please put your age: '))
+#         degree = input('Please put your degree level: ')
+#         student_list[i]['Name'] = name
+#         student_list[i]['Age'] = age
+#         student_list[i]['Degree'] = degree
+#         print(student_list[i])
+#     # print(student_list)
+#     continue_function = input('Do you wanna add another student records? (y/n)')
+#     if continue_function == 'y':
+#         record_numbers = int(input("How many records do you wanna add?"))
+
+    
+# pprint(student_list)
+# print(student_list)
+# del student_list[5]
+# print(student_list)
+
+print
