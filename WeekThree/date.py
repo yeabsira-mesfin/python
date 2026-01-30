7e70375707558fa83a085a6f344a42cfd1789789
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-01-30 14:04:03 -0500

    Calendar

---
+++ WeekThree/date.py
@@ -0,0 +1,48 @@
+import time
+import calendar
+# import datetime
+# from time import *
+# # localtime = time.localtime(time.time())
+# # print(localtime)
+
+# formatedtime = asctime(localtime(time()))
+# print(formatedtime)
+# formated_date =  datetime.datetime.now()
+# print(f"""
+#      The formatted date is {formated_date}
+#      The year is  {datetime.date.today().strftime("%Y")}
+#      The month is {datetime.date.today().strftime("%B")}
+#      The week number of the year is {datetime.date.today().strftime("%W")}
+#      The day of the year is {datetime.date.today().strftime("%w")}
+#      The week is {datetime.date.today().strftime("%j")}
+#      The day is {datetime.date.today().strftime("%d")}
+#      The day is {datetime.date.today().strftime("%A")}""")
+
+# """
+# yy-mm-dd format
+# """
+
+# print(f"""
+#       Current date and time: {datetime.datetime.now()}
+#       yy-mm-dd fromat {datetime.date.today().strftime("%Y-%m-%d")}
+#       dd-mm-yy fromat {datetime.date.today().strftime("%d-%m-%Y")}
+#       """)
+# t = time.localtime()  
+# print (time.asctime(t))
+
+# calendar.prcal(2026)
+
+## monthly calendar
+monthc = calendar.month(2026,7)
+print(f""" This month calendar:: 
+      {monthc} """)
+
+y = int(input("Enter the year and will tell you if it's leap year or not: "))
+if calendar.isleap(y):
+    print(f"{y} is leap year")
+else:
+    print(f"{y} is not a leap year.")
+# if (y %4 == 0 and y%100!=0) or y %400 == 0:
+#     print(f"{y} is leap year")
+# else:
+#     print(f"{y} is not a leap year")
