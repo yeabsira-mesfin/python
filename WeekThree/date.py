82e5fbc8a7079baa76b8bfe3e9453a3c3f88eec7
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-01-30 14:19:36 -0500

    date

--- WeekThree/date.py
+++ WeekThree/date.py
@@ -1,5 +1,6 @@
-import time
-import calendar
+# import time
+# import calendar
+from datetime import *
 # import datetime
 # from time import *
 # # localtime = time.localtime(time.time())
@@ -33,16 +34,32 @@ import calendar
 # calendar.prcal(2026)
 
 ## monthly calendar
-monthc = calendar.month(2026,7)
-print(f""" This month calendar:: 
-      {monthc} """)
+# monthc = calendar.month(2026,7)
+# print(f""" This month calendar:: 
+#       {monthc} """)
 
-y = int(input("Enter the year and will tell you if it's leap year or not: "))
-if calendar.isleap(y):
-    print(f"{y} is leap year")
-else:
-    print(f"{y} is not a leap year.")
+# y = int(input("Enter the year and will tell you if it's leap year or not: "))
+# if calendar.isleap(y):
+#     print(f"{y} is leap year")
+# else:
+#     print(f"{y} is not a leap year.")
 # if (y %4 == 0 and y%100!=0) or y %400 == 0:
 #     print(f"{y} is leap year")
 # else:
 #     print(f"{y} is not a leap year")
+
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# print(calendar.month(yy,mm))
+
+d0 = date(1999,7,6)
+d1 = date(2026,1,30)
+
+diff = d1-d0
+diffy = d0.year - d1.year
+
+print(diff)
+print(f"""
+      {diffy}
+      """)
