cdf6056680e070c385cead21a771810102d5c570
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-02-02 13:37:36 -0500

    Math

---
+++ WeekTwo/math.py
@@ -0,0 +1,15 @@
+# import math
+# # from math import *
+# from math import sin, tan, tan
+# # print(type(math))
+# # print(math.radians(45))
+# # print(f" Sin value= {math.sin(30)}")
+# # print(f" Cos vlaue= {math.cos(30)}")
+# # print(f" Tan value= {math.tan(45)}")
+# print(f"Sin value= {sin(30)}")
+# print(f"Tan value= {tan(45)}")
+x=888
+def add(a,b):
+	print("The Sum:",(a+b))
+def product(a,b):
+	print("The product:",(a*b))
---
+++ WeekTwo/weektestwrap.py
@@ -0,0 +1,31 @@
+import re
+import time
+# for x in [2,4,6,7,8]:
+#     if x%2 != 0:
+#         print("Odd number found")
+#         break
+#     else:
+#         print("No odd number")
+# a = [1,2]
+# b = a
+# c = [1,2]
+# a += [3]
+# print(a,b,c)
+# print(a is b, a == c)
+# def is_armstrong(n: int) -> bool:
+#     s = str(n)
+#     k = len(s)
+#     total = 0
+#     for ch in s:
+#         total += int(ch) ** k
+#     return total == n
+# print(is_armstrong(153))
+# def is_valid_code(value:str, pattern: str) -> bool:
+#     if re.fullmatch(pattern,value):
+#         return True
+#     else:
+#         return False
+    
+# print(is_valid_code("Yeab","[A-Za-z]+\\d{1}"))
+
+print(time.time())
