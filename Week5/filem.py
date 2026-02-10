9cd449071bf36c7e9ba6c3d4f7c232553e97dbc5
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-02-10 12:36:38 -0500

    reading from a file

---
+++ Week5/filem.py
@@ -0,0 +1,6 @@
+f = open("D:\\Python\\MyOwnPractices\\python\\Week5\\data2.txt", "w")
+f.write("This first line \n")
+f.write("This second line \n")
+print("1 file created: ")
+f.close()
+
---
+++ Week5/readingfromfile.py
@@ -0,0 +1,3 @@
+f = open("D:\\Python\\MyOwnPractices\\python\\Week5\\data.txt", "r")
+print(f.read())
+f.close()
