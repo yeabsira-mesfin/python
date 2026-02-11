40189aab10f51664a5dd562ade44cf7ff2eb0a20
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-02-11 12:25:23 -0500

    w+ and r+

--- Week5/append.py
+++ Week5/append.py
@@ -1,10 +1,23 @@
-f = open("D:\\Python\\MyOwnPractices\\python\\Week5\\data3.txt", "a")
-s1 = (input("Please enter anyk string: "))
-# binary_text = s1.encode("utf-8")
-f.write(s1)
-f.close()
+# f = open("D:\\Python\\MyOwnPractices\\python\\Week5\\data3.txt", "a")
+# s1 = (input("Please enter anyk string: "))
+# # binary_text = s1.encode("utf-8")
+# f.write(s1)
+# f.close()
 
 
-f = open("D:\\Python\\MyOwnPractices\\python\\Week5\\data3.txt", "rb")
-print(f.read())
-f.close()
+# f = open("D:\\Python\\MyOwnPractices\\python\\Week5\\data3.txt", "rb")
+# print(f.read())
+# f.close()
+def main():
+    f = open("D:\\Python\\MyOwnPractices\\python\\Week5\\data3.txt", "w+")
+    for i in range(10):
+        f.write(f"This is line {i} \n")
+    f.seek(0)
+    # f = open("D:\\Python\\MyOwnPractices\\python\\Week5\\data3.txt", "r")
+    contents = f.read()
+    print(contents)
+    f.close()
+
+    
+if __name__ == "__main__":
+    main()
