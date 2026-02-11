b0dea8e5aab895da6f988a34df96d7aeeb76636c
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-02-11 13:30:25 -0500

    shutil

--- Week5/os.py
+++ Week5/os.py
@@ -1,10 +1,21 @@
 import os
-drt = "D:\\Python\\MyOwnPractices\\python\\Week5\\test"
+import shutil
+# drt = "D:\\Python\\MyOwnPractices\\python\\Week5\\TestOne"
 
 # os.mkdir(drt)
 # print(f"{os.path.basename(drt)} is created")
 
-newdrti = input("Please enter the directory name you want: ")
-newdrt = f"D:\\Python\\MyOwnPractices\\python\\Week5\\{newdrti}"
-os.rename(drt,newdrt)
-print(f"{os.path.basename(newdrt)} is created: ")
+# newdrti = input("Please enter the directory name you want: ")
+# newdrt = f"D:\\Python\\MyOwnPractices\\python\\Week5\\{newdrti}"
+# os.rename(drt,newdrt)
+# print(f"{os.path.basename(newdrt)} is created: ")
+# print(os.getcwd())
+# drt = "D:\\Python\\MyOwnPractices\\python\\Week5\\TestOne"
+# os.rmdir(drt)
+# print(f"{os.path.basename(drt)} is removed")
+# t = open("D:\\Python\\MyOwnPractices\\python\\Week5\\TestOne\\test1.txt","w")
+# t2 = "D:\\Python\\MyOwnPractices\\python\\Week5\\TestOne\\test1.txt"
+# print(f"{os.path.basename(t2)} is created")
+removed = "D:\\Python\\MyOwnPractices\\python\\Week5\\TestOne"
+shutil.rmtree(removed)
+print(f"{os.path.basename(removed)} is removed")
