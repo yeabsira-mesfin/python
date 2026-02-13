96e3bb7193ff8b5efe33bd0faf93e0e2f46b1c26
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-02-13 17:51:41 -0500

    gui

--- Week5/Tkinter/gui1.py
+++ Week5/Tkinter/gui1.py
@@ -101,5 +101,15 @@ from tkinter.ttk import *
 # chk.grid(column=0,row=0)
 # window.mainloop()
 
+window = Tk()
+window.title("This is the app!")
+window.geometry("350x400")
+rad1 = Radiobutton(window,text="First",value=1)
+rad2 = Radiobutton(window,text="Second",value=2)
+rad3 = Radiobutton(window,text="Third",value=3)
 
+rad1.grid(column=0,row=0)
+rad2.grid(column=1, row=0)
+rad3.grid(column=3,row=0)
 
+window.mainloop()
