3a12bd5ea5f47dd34ea5c19eed478e98c1cb0335
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2026-02-19 12:50:01 -0500

    test

--- Week6/test.py
+++ Week6/test.py
@@ -1,10 +1,24 @@
-import socket
+def withdraw(balance,amount):
+      
+      if not isinstance(balance,(int, float)):
+            raise TypeError("Balance must be a number")
+      
+      if not isinstance(amount, (int, float)):
+            raise TypeError("Amount must be a number")
+      
+      if amount <= 0:
+            raise ValueError("Amount must be greater than 0")
+      
+      if balance < amount:
+            raise ValueError("Balance must be greater than 0")
+      if (amount < 0):
+            raise ValueError("Amount must be greater than 0")
+      
+      return balance-(amount)
 
-host_name = socket.gethostname()
-ip_address = socket.gethostbyname(host_name)
-
-print(f"""
-This is the host name: {host_name}
-This is the ip address: {ip_address} 
-      """)
+withdraw(500,56)
+withdraw("500",56)
+withdraw(500,"56")
+withdraw(500,560)
+withdraw(555,-55)
 
